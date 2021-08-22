064de576b109d729890046b6744b930e014bd8b2
AleksVersus <aleksversus@mail.ru>
2021-08-22 21:02:25 +0500

    Доведена до ума конвертация строки на примере title. Продолжаем.

--- ответы/foo.py
+++ ответы/foo.py
@@ -113,41 +113,107 @@ def getStringList(files_list):
 def ampersandReplace(string_):
 	# замена амперсандов в строках
 	while True:
-		if re.search(r'\&(?!\w+;)',string_)!=None:
-			string_=re.sub(r'\&(?!\w+;)','&amp;',string_)
+		if re.search(r'\&(?!\S+?;)',string_)!=None:
+			string_=re.sub(r'\&(?!\S+?;)','&amp;',string_)
 		else:
 			break
 	return string_
+def anglebrackReplace(string_):
+	string_=string_.replace('<','&lt;')
+	string_=string_.replace('>','&gt;')
+	return string_
+
+def replaceAll(string_):
+	string_=ampersandReplace(string_)
+	string_=anglebrackReplace(string_)
+	return string_
+
 class NewString():
 	def __init__(self,string_,**args_):
 		self.type="" # по типу определяем, что это за строка (например <title>)
 		self.string_body=[] # список объектов
-		if args_['start']==True:
-			# заголовок рассматривается только если строка приходит первый раз
-			title=re.match(r'^\s*(<title>)(.*?)(</title>)\s*$',string_) # заголовок ли?
-		else:
-			title=None
-		hyperlink=re.match(r'^\[[^\]]+\]\([^\)]*\)$',string_) # есть ли гиперссылки
-		if title!=None:
+		self.last_string="" # если объекты уже нельзя сгенерировать, оставляем строку здесь
+		if not 'strtype' in args_:
+			args_['strtype']=''
+		if args_['strtype']=='title':
 			# если это строка заголовок
 			self.type='title'
-			string_=re.match(r'^\s*(<title>)(.*?)(</title>)\s*$',string_)[0][1]
-			string_body.append(NewString(string_))
-		elif hyperlink!=None:
+			string_=re.findall(r'^\s*(<title>)(.*?)(</title>)\s*$',string_)[0][1]
+			self.string_body.append(NewString(string_))
+		elif args_['strtype']=='hyperlink':
 			# если строка является гиперссылкой
-def convertString(string_):
-	title_string=[]
-	varname=NewString(string_,start=True)
-	link_list=re.findall(r'\[[^\]]+\]\([^\)]*\)',title)
-	for link in link_list:
-		symbol=title.find(link)
-		title_string.append(title[0:symbol])
-		title_string.append(title[symbol:symbol+len(link)])
-		title=title[symbol+len(link):]
-	title=""
-	for i in title_string:
-		title+=f"{i}\n"
-	return title
+			self.type="hyperlink"
+			hl_text,hl_href=re.findall(r'^\[(.*?)\]\((.*?)\)$',string_)[0]
+			self.string_body.append(NewString(hl_text)) # текст ссылки в нулевой ячейке
+			self.string_body.append(NewString(hl_href,strtype='href')) # адрес в первой
+		elif args_['strtype']=='href':
+			self.type='href'
+			self.last_string=string_
+		elif args_['strtype']=='monotype':
+			self.type='monotype'
+			self.last_string=string_
+		else:
+			hyperlink=re.search(r'\[(.*?)\]\((.*?)\)',string_)
+			monotype=re.search(r'`[^`]+`',string_)
+			if hyperlink!=None:
+				# если в тексте пристутсвуют гиперссылки, разбиваем текст на части
+				link_list=re.findall(r'\[[^\]]+\]\([^\)]*\)',string_)
+				for link in link_list:
+					symbol=string_.find(link)
+					self.string_body.append(NewString(string_[0:symbol])) # простая строка
+					self.string_body.append(NewString(string_[symbol:symbol+len(link)],strtype='hyperlink')) # гиперссылка
+					string_=string_[symbol+len(link):]
+				if len(string_)>0: self.string_body.append(NewString(string_))
+			elif monotype!=None:
+				# если в тексте присутствует monotype разметка
+				# определяем, можно ли на лету перекрасить эту разметку
+				mt_list=re.findall(r'`\w+`\w+\b',string_)
+				for i in mt_list:
+					y_name,y_sub=re.findall(r'`(\w+?)`(\w+)\b',i)[0]
+					string_=string_.replace(i,f"{y_name}'{y_sub}")
+				monotype=re.search(r'`[^`]+`',string_)
+				if monotype!=None:
+					mt_list=re.findall(r'`[^`]+`',string_)
+					for i in mt_list:
+						symbol=string_.find(i)
+						self.string_body.append(NewString(string_[0:symbol])) # простая строка
+						self.string_body.append(NewString(string_[symbol:symbol+len(link)],strtype='monotype'))
+						string_=string_[symbol+len(link):]
+					if len(string_)>0: self.string_body.append(NewString(string_))
+			else:
+				self.last_string=string_
+	def getString(self):
+		if len(self.last_string)>0:
+			string_=replaceAll(self.last_string)
+			if self.type=='title':
+				return f'<title>{string_}</title>'
+			elif self.type=='href':
+				return f'<a href="{self.last_string}">'
+			elif self.type=='monotype':
+				return f'<strong>{string_}</strong>'
+			else:
+				return string_
+		else:
+			if self.type=='title':
+				string_="<title>"
+				for i in self.string_body:
+					string_+=i.getString()
+				string_+='</title>'
+				return string_
+			elif self.type=='hyperlink':
+				return f'{self.string_body[1].getString()}{self.string_body[0].getString()}</a>'
+			else:
+				string_=""
+				for i in self.string_body:
+					string_+=i.getString()
+				return string_
+
+def convertString(string_,**args_):
+	if not 'strtype' in args_:
+		args_['strtype']=''
+	varname=NewString(string_,strtype=args_['strtype'])
+	return varname.getString()
+
 	
 def convertationFB2(body_list):
 	# данная функция конвертирует перворазбитый список строк в готовый документ fb2
@@ -162,8 +228,10 @@ def convertationFB2(body_list):
 		elif section!=None:
 			new_string_list.append(string_)
 		elif title!=None:
-			new_string_list.append(convertString(string_))
+			new_string_list.append(convertString(string_,strtype='title'))
 	return new_string_list
 
 if __name__=="__main__":
-	print(convertTitle("<title>[название ссылки](#link_01_01)Есть просто символ &. Есть в слове &ghjk. А есть &nbsp; [https://www.youtube.com/watch?v=uaEfDzBXFSw&](https://www.youtube.com/watch?v=uaEfDzBXFSw&list=PLcAHO4WsUl2R70UDclnyevDSuBJGDH9F6&index=1&t=25s) ещё часть строки</title>"))
+	string="<title>[название ссылки](#link_01_01)Есть <просто> символ &. Есть в слове &ghjk. А есть &nbsp; [https://www.youtube.com/watch?v=uaEfDzBXFSw&](https://www.youtube.com/watch?v=uaEfDzBXFSw&list=PLcAHO4WsUl2R70UDclnyevDSuBJGDH9F6&index=1&t=25s) ещё часть строки</title>"
+	print(string)
+	print(convertString(string,strtype='title'))
