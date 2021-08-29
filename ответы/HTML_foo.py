1be9c710182c32c170588412cfabc7088f174a35
AleksVersus <aleksversus@mail.ru>
2021-08-29 23:51:58 +0500

    Перенос NewBD в функции

--- ответы/HTML_foo.py
+++ ответы/HTML_foo.py
@@ -13,6 +13,100 @@ def dirList(folder_path):
 		elif os.path.isdir(folder_path+"\\"+path):
 			folders_list.append(folder_path+"\\"+path)
 	return files_list, folders_list
+class NewBD():
+	"""База данных представляет собой список файлов с их идентификаторами:
+	путь, уникальный идентификатор, идентификатор в привязке к структуре"""
+	def __init__(self):
+		self.base=[] # сама по себе база является списком списков
+		self.filecount=0
+		self.addition=[] # добавочная секция в файл
+	def proveAdd(self):
+		if len(self.addition)!=0:
+			return True
+		else:
+			return False
+	def addAdd(self,source):
+		self.addition.extend(source)
+	def delAdd(self):
+		self.addition=[]
+	def getLastID(self):
+		# получаем последний айди
+		return '0'*(8-len(str(self.filecount)))+str(self.filecount)
+	def addFile(self, path,**args):
+		# добавляем файл
+		if not "section_id" in args:
+			args["section_id"]=''
+		file=[path,self.getLastID(),args["section_id"]]
+		self.base.append(file)
+		self.filecount+=1
+	def getBase(self):
+		return self.base
+	def searchFile(self,**args):
+		result=None
+		for file_path,file_id,section_id in self.base:
+			if ("section_id" in args) and (section_id==args["section_id"]):
+				result=[file_path,file_id,section_id]
+				break
+			if ("file_id" in args) and (file_id==args["file_id"]):
+				result=[file_path,file_id,section_id]
+				break
+			if ("file_path" in args) and (file_path==args["file_path"]):
+				result=[file_path,file_id,section_id]
+				break
+		if "result" in args:
+			# если мы хотим получить какой-то определённый результат
+			if args["result"]=="file_path":
+				return result[0]
+			if args["result"]=="file_id":
+				return result[1]
+			if args["result"]=="section_id":
+				return result[2]
+			if args["result"]=="index":
+				return self.base.index(result)
+		else:
+			return result[0]
+	def getID(self,path):
+		return self.searchFile(file_path=path,result='section_id')
+	def changeID(self,path,section_id):
+		idx=self.searchFile(file_path=path,result='index')
+		self.base[idx][2]=section_id
+class NewSection():
+	"""Части файла представляют собой блоки"""
+	def __init__(self):
+		self.type="" # тип блока
+		self.id="" # id блока
+		self.source=[] # список строк блока
+		self.HTML=[] # html форма секции
+	def addString(self,string):
+		self.source.append(string)
+	def changeType(self,type_):
+		self.type=type_
+	def changeID(self,id_):
+		self.id=id_
+	def getLen(self):
+		return len(self.source)
+	def getAttr(self,**args):
+		if not "attr" in args:
+			args["attr"]='source'
+		if args['attr']=='type':
+			return self.type
+		if args['attr']=='id':
+			return self.id
+		if args['attr']=='source':
+			return self.source
+	def getType(self):
+		return self.getAttr(attr='type')
+	def getSource(self):
+		return self.getAttr(attr='source')
+	def __str__(self):
+		return f"'{self.type},{self.id},{len(self.source)},{len(self.HTML)},{type(self)}'\n"
+	def convert2HTML(self,base):
+		if self.type=="":
+			type_='string'
+			# имеем дело с параграфом. Каждая строка - отдельный параграф
+			for string in self.source:
+				self.HTML.append(convertString(string,type_,base))
+
 def remComment(string_):
 	mode_=False
 	start_string=''
@@ -109,9 +203,7 @@ class NewString():
 		self.type=type_
 		self.strings=[]
 		print(f"'type:{self.type},string: {string_}'")
-		if self.type=='':
-			pass
-		elif self.type=='string':
+		if self.type=='string':
 			termins=re.match(r'^\s*?`.*?`(,\s+`.*?`)*\s*—',string_)
 			if termins!=None:
 				word1=termins.group(0)
@@ -132,6 +224,42 @@ class NewString():
 				string_=string_[symbol+len(term):]
 			if len(string_)>0:
 				self.strings.append(NewString(string_,'',base_))
+		elif self.type=='term':
+			self.strings.append(NewString(string_,'',base_))
+		elif self.type=='hyperlink':
+			hl_text,hl_href=re.findall(r'^\[(.*?)\]\((.*?)\)$',string_)[0]
+			self.strings.append(NewString(hl_text,'',base_)) # текст ссылки в нулевой ячейке
+			if re.match(r'^#',hl_href)!=None:
+				hl_href='#folder#'+base_.searchFile(section_id=hl_href[1:],result='file_id')+'.html'+hl_href
+			self.strings.append(NewString(hl_href,'href',base_)) # адрес в первой
+		else:
+			id_word=re.search(r'\[:[^\]]*?\]',string_)
+			link_word=re.search(r'\[.*?\]\(.*?\)',string_)
+			if id_word!=None:
+				words=re.findall(r'\[:[^\]]*?\]',string_)
+				for word in words:
+					symbol=string_.find(word)
+					word1=string_[0:symbol]
+					word2=string_[symbol:symbol+len(word)]
+					if word1!="":
+						self.strings.append(NewString(word1,'',base_))
+					self.strings.append(NewString(word2,'id',base_))
+					string_=string_[symbol+len(word):]
+				if len(string_)>0:
+					self.strings.append(NewString(string_,'',base_))
+			if link_word!=None:
+				words=re.findall(r'\[.*?\]\(.*?\)',string_)
+				for word in words:
+					symbol=string_.find(word)
+					word1=string_[0:symbol]
+					word2=string_[symbol:symbol+len(word)]
+					if word1!="":
+						self.strings.append(NewString(word1,'',base_))
+					self.strings.append(NewString(word2,'hyperlink',base_))
+					string_=string_[symbol+len(word):]
+				if len(string_)>0:
+					self.strings.append(NewString(string_,'',base_))
+
 def convertString(string_,type_,base_):
 	# конвертируем строку в параграф
 	roof_string=NewString(string_,type_,base_)
@@ -141,4 +269,6 @@ def convertString(string_,type_,base_):
 
 if __name__=="__main__":
 	string=f'`Обработка локации`, `посещение локации` — под этими терминами понимаются следующие процессы: выполнение кода из поля "Выполнить при посещении" указанной локации, добавление в окно основного описания. `goto` `xgoto` Всякое такое. ["Разница между `goto` и `gosub`"](#faq_01_08). Спасибо `Nex`у. `Larson`у.'
-	NewString(string,'string',[1,2,3])
+	roof_base=NewBD()
+	roof_base.addFile('path',section_id='faq_01_08')
+	NewString(string,'string',roof_base)
