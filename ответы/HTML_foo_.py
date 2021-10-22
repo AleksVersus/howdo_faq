e8b465ac98b7ddf9e462cfdaf569fb96300771bf
AleksVersus <aleksversus@mail.ru>
2021-10-22 20:29:31 +0500

    Реорганизация модулей. Тупик с файлом.

--- ответы/HTML_foo_.py
+++ ответы/HTML_foo_.py
@@ -20,3 +20,41 @@ def randomString(length):
 	letters+='ЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮёйцукенгшщзхъфывапролджэячсмитьбю'
 	result = ''.join(random.choice(letters) for i in range(length))
 	return result
+
+def clearStringList(string_list):
+	# удаляем комментарии везде, кроме блоков кода
+	mode={"comment":False,"code":False}
+	new_string_list=[]
+	for string_ in string_list:
+		comment=re.match(r'(.*?/\*.*)|(.*?\*/.*)',string_)
+		code=re.match(r'^\s*?```\w*\s*$',string_)
+		if mode["comment"]==True and comment==None:
+			pass # строки комментария не добавляются в новый список
+		elif mode["code"]==True and code==None:
+			# строки в блоках кода добавляются без изменений
+			new_string_list.append(string_) 
+		elif comment!=None and mode["code"]==False:
+			# если строка содержит комментарий, а режим кода выключен
+			# изменяем режим и получаем строку без комментария
+			mode["comment"],string_=remComment(string_)
+			if re.match(r'^\s+?$',string_)!=None:
+				# если строка состоит из одних пробелов, это пустая строка
+				string_=""
+			if string_!="" and string_!=None:
+				new_string_list.append(string_) 
+		elif code!=None and mode["comment"]==False:
+			# если строка содержит границу блока кода, но не включён режим комментария
+			if mode["code"]==False:
+				mode["code"]=True
+			else:
+				mode["code"]=False
+			# строка при этом добавляется в выходной список
+			if re.match(r'^\s+?$',string_)!=None:
+				# если строка состоит из одних пробелов, это пустая строка
+				string_=""
+			if string_!="" and string_!=None:
+				new_string_list.append(string_) 
+		else:
+			if string_!=None:
+				new_string_list.append(string_) 
+	return new_string_list
--- ответы/HTML_lib.py
+++ ответы/HTML_lib.py
@@ -21,18 +21,21 @@ class NewDataBase():
 	def currentFile(self):
 		# получаем идентификатор текущего файла
 		return self.curfile
+	def setCurFile(self,id_):
+		# устанавливаем текущий файл. Выбираем его айди
+		self.curfile=id_
 	def proveAdd(self):
 		# присутствует ли в базе добавочная секция
-		if len(self.addition)!=0:
-			return True
-		else:
-			return False
+		return (True if len(self.addition)!=0 else False)
 	def addAdd(self,string_list):
 		# добавляем в базу добавочную секцию
 		self.addition.extend(string_list)
 	def delAdd(self):
 		# удаляем из базы добавочную секцию
 		self.addition=[]
+	def getAdd(self):
+		# получаем из базы добавочную секцию
+		return self.addition[:1]
 	def lastFileID(self):
 		# получаем последний айди файлов
 		if self.filecount==0:
@@ -44,7 +47,7 @@ class NewDataBase():
 		return '0'*(8-len(str(self.filecount)))+str(self.filecount)
 	def addFile(self,path):
 		# добавление нового файла
-		id_=self.genNewID() # генерируем идентификатор файла
+		id_=self.genNewFileID() # генерируем идентификатор файла
 		self.filecount+=1 # увеличиваем счётчик
 		self.files_dict[path]=id_ # вносим файл в словарь
 		self.curfile=id_ # текущим файлом становится добавленный
@@ -56,19 +59,18 @@ class NewDataBase():
 			print("Ошибка! Текущий Файл не определён. Якорь <<anchor>> не добавлен")
 	def getFileID(self,path):
 		# получаем айди файла по указанному пути
-		return self.files_dict[path]
+		return (self.files_dict[path] if path in self.files_dict else None)
 	def getFilePath(self,id_):
 		# получаем путь к файлу по ай-ди
+		result=None
 		for key in self.files_dict:
 			if self.files_dict[key]==id_:
-				return key
+				result=key
 				break
+		return result
 	def getFileName(self,anchor):
 		# получаем айди файла по содержащемуся в нём якорю
-		if anchor in self.anchors_dict:
-			return self.anchors_dict[anchor]
-		else:
-			return ''
+		return (self.anchors_dict[anchor] if anchor in self.anchors_dict else None)
 	def addHeader(self,string_list):
 		self.header_list=string_list[:]
 	def addFooter(self,string_list):
@@ -102,4 +104,35 @@ class NewFolder():
 		for file in files_list:
 			self.files.append(NewFile(file,base))
 	def convert2HTML(self,base):
+		pass
+
+class NewFile():
+	"""
+	Каждый файл представляет собой контейнер для секций.
+	"""
+	def __init__(self,path,base):
+		self.path=path # путь к файлу является его уникальным идентификатором
+		# получаем список строк, это и есть файл
+		with open(path,'r',encoding='utf-8') as file:
+			self.source=file.readlines()
+		self.sections=[] # блоки составляющие файл
+		self.HTML=[] # список строк готового HTML-файла
+		if base.proveAdd():
+			self.source=base.getAdd()+self.source
+		if self.getFileName()=='00.txt-light':
+			# если это файл заголовка, этот заголовок будет добавляться ко всем последующим файлам
+			base.addAdd(self.source)
+		else:
+			# если это не файл заголовка
+			base.addFile(path) # добавляем файл в базу
+			self.sections.append(NewSection(self.source,'',base))
+			self.convert2HTML(base)
+	def getFileName(self):
+		# получаем имя файла, отсекая путь
+		return os.path.split(self.path)[1]
+	def convert2HTML(self,base):
+		# конвертируем секции файла в HTML
+		for section in self.sections:
+			section.convert2HTML(base)
+	def getHTML(self,base):
 		pass
