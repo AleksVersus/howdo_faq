6a98c6ddb8f1d3ce98045eb0848b617edf5c731d
aleksversus <aleksversus@mail.ru>
2022-09-04 16:51:44 +0500

    исправлен скрипт так, чтобы мы вешали событие на элемент

--- res/js/soderzhanie_1.js
+++ res/js/soderzhanie_1.js
@@ -4,17 +4,21 @@
     function close_content(event) {
         // в функцию передаём событие event у события есть цель, эта цель получается с помощью свойства .target
         // таким образом из события мы можем восстановить элемент к которому это событие применяется
+        var sdr = document.getElementById('soderzhanije_outer'); // здесь мы получаем элемент, на который хотим воздействовать
         // далее сравниваем айдишники элементов, к которым применяется событие с нужными нам. И если событие применяется не
         // к тому элементу, игнорируем этот элемент. Если же событие применяется к нужному нам элементу, скрываем содержание
         if ((event.target.id === "soderzhanije_outer") || (event.target.id === "section_content_close")) {
-            document.getElementById('soderzhanije_outer').style.cssText = "display:none;";
+            sdr.style.cssText = "display:none;";
         }
     }
     function open_content() {
-        document.getElementById('soderzhanije_outer').style.cssText = "display:flex;";
+        var sdr = document.getElementById('soderzhanije_outer');
+        sdr.style.cssText = "display:flex;";
     }
-    // вешаем слушателей событий на элементы
-    document.getElementById('section_content_open').addEventListener('click', open_content);
-    document.getElementById('section_content_close').addEventListener('click', close_content);
-    document.getElementById('soderzhanije_outer').addEventListener('click', close_content);
+    var btn_open = document.getElementById('section_content_open');
+    btn_open.onclick = open_content;
+    var btn_close = document.getElementById('section_content_close');
+    btn_close.onclick = close_content;
+    var sdr = document.getElementById('soderzhanije_outer');
+    sdr.onclick = close_content;
 }());
