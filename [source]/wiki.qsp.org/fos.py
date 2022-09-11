adaec0a574866024788a13874c70b0ba51f3061e
aleksversus <aleksversus@mail.ru>
2022-09-12 01:39:58 +0500

    Переразмещение разделов, правка start, wtf_qsp

--- [source]/wiki.qsp.org/fos.py
+++ [source]/wiki.qsp.org/fos.py
@@ -187,7 +187,7 @@ def main_rename(folder):
 
 if __name__=="__main__":
 	counter=-1
-	folder = NewFolder("D:\\my\\projects\\howdo_faq\\[source]\\wiki.txt\\11_utilits_tools")
+	folder = NewFolder("D:\\my\\projects\\howdo_faq\\[source]\\wiki.txt\\07_programming")
 	folder.renameFiles(filt=r"(^.{3,}\.txt-light$|^[^0]{2}$)/re",mask="[CC]_[N4].[E]",level=1)
 	# main_rename(folder) # пакетное переименовывание всех файлов
 	# print(folder.foundCount(start=True,filt=r"(^.{3,}\.txt-light$|^[^0]{2}$)/re")) # поиск конечного счётчика
