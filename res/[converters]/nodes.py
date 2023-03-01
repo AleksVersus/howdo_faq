6f9ab49eafefc2e7a95df2e1e0db55ea9168e562
aleksversus <aleksversus@mail.ru>
2023-03-01 18:46:53 +0500

    Добито разложение на узлы. Надеюсь ничего не упустил.

--- res/[converters]/nodes.py
+++ res/[converters]/nodes.py
@@ -165,14 +165,23 @@ class NewNode():
 			self.head_stn()
 		elif self.node_type == 'list-node':
 			self.list_stn()
+		elif self.node_type == 'code':
+			self.code_stn()
+		elif self.node_type == 'string':
+			self.string_stn()
 
-	def create_node(self, string_lines, node_type='segment'):
+	def create_node(self, string_lines, node_type='segment', attributes=None):
 		if len(string_lines)>0:
 			node = NewNode(node_type=node_type)
 			node.add_source(string_lines)
 			node.source_to_nodes()
+			if attributes in not None: node.attributes.update(attributes)
 			string_lines = []
 			self.add_node(node)
+
+	def creat_simple_string(self, string_line:str):
+		if len(string_line)>0:
+			self.create_node([string_line], node_type='tag', attributes={'name': 'simple-string'})
 
 	def segment_stn(self):
 		# break node on big nodes
@@ -233,12 +242,12 @@ class NewNode():
 		else:
 			# If node not broke on segments-nodes, break node in other types nodes.
 			# Manage information:
-			block_mode='' # выставляем режимы добора в секции
+			block_mode='' # mode of addition in blocks
 			manage_mode={
 
-				# "string-type": ("block-types") 
-				"head":('','quote-list','ul-ol-list','head-block'), # блоки, которые можно закрывать строкой заголовка
-				"id": ('head-block','quote-list'), # блоки, которые можно закрывать строкой с айди
+				# "string-type": ("which block-types it is may close.") 
+				"head":('','quote-list','ul-ol-list','head-block'),
+				"id": ('head-block','quote-list'),
 				"ul-ol":('','quote-list','head-block'),
 				"code":('','code-block','head-block','quote-list'),
 				"quote":('','quote-block','head-block','quote-list'),
@@ -254,7 +263,7 @@ class NewNode():
 				'ul-ol-list': 'list-node',
 				'code-block': 'code'
 			}
-			mode = {'code-block-open': False}
+			mode = {'code-block-open': False, 'code-type': None}
 			string_lines = []
 			for line in self.string_lines:
 				string_type = self.string_type(line)
@@ -266,11 +275,17 @@ class NewNode():
 						string_lines.append(line)
 				elif string_type=='code':
 					if block_mode=='code-block':
-						self.create_node(string_lines, node_type=block_to_node[block_mode])
+						self.create_node(
+							string_lines,
+							node_type=block_to_node[block_mode],
+							attributes={'code-type': mode['code-type']}
+						)
 						block_mode=''
+						mode['code-type'] = None
 					elif block_mode in manage_mode["code"]:
 						self.create_node(string_lines, node_type=block_to_node[block_mode])
-						block_mode="code-block"
+						block_mode = "code-block"
+						mode['code-type'] = self.get_code_type(line)
 					else:
 						string_lines.append(line)
 					mode['code-block-open'] = not mode['code-block-open']
@@ -338,7 +353,11 @@ class NewNode():
 		for line in self.string_lines:
 			string_type = self.string_type(line)
 			string_level = self.get_string_level(line)
-			if string_type in ('ul-li', 'ol-li') and string_level == top_list_level and not mode['code-block-open']:
+			if all((
+					string_type in ('ul-li', 'ol-li'),
+					string_level == top_list_level,
+					not mode['code-block-open']
+				)):
 				if self.all_modes_same(mode):
 					if string_type=='ul-li':
 						mode['ul-block-open']=True
@@ -355,11 +374,186 @@ class NewNode():
 				string_lines.append(line)
 		self.create_node(string_lines)
 
+	def code_stn(self):
+		# Code-block is prepared node for convertion.
+		# Clear the strings from space.
+		top_level = 999999
+		string_lines = []
+		while len(self.string_lines)>0:
+			line = self.string_lines.pop(0).replace('\t', ' '*4)
+			level = self.get_string_level(line)
+			top_level = (level if level<top_level else top_level)
+			string_lines.append(line)
+		regex = re.compile(r'^\s{'+str(top_level)+r'}([\s\S]*)')
+		while len(string_lines)>0:
+			line = re.match(regex, string_lines.pop(0)).group(1)
+			self.string_lines.append(line)
 
-
-
+	def string_stn(self):
+		if len(self.string_lines)>0:
+			line = self.string_lines.pop(0)
+			while len(line)>0:
+				scope_type, scope, line = find_separate_scope(line)
+				if scope_type=='anchor':
+					self.node_type='tag'
+					self.attributes={
+						'name': 'anchor',
+						'anchor': self.extract_anchor(scope)
+					}
+				elif scope_type=='hr':
+					self.node_type='tag'
+					self.attributes={'name': 'hr'}
+				elif scope_type=='image':
+					text, href = self.extract_href(scope)
+					self.node_type='tag'
+					self.attributes={
+						'name': 'image',
+						'src': href
+					}
+				else:
+					scope_type, prev_line, scope, line = self.find_overlap_scope(line)
+					if scope_type is None:
+						# simple-string
+						self.node_type='tag'
+						self.attributes={'name': 'simple-string'}
+						self.string_lines.append(line)
+						line=''
+					elif scope_type=='hyperlink':
+						self.creat_simple_string(prev_line)
+						text, href = self.extract_href(scope)
+						self.create_node(
+							[text],
+							node_type='tag',
+							attributes={'name': 'hyperlink', 'href': href}
+						)
+					elif scope_type in ('tt-quote', 'back-apostroph'):
+						self.creat_simple_string(prev_line)
+						scope = self.extract_tt_quote(scope, scope_type)
+						self.create_node(
+							[scope],
+							node_type='tag',
+							attributes={'name': 'tt'}
+						)
+					elif scope_type=='bold-italic':
+						self.creat_simple_string(prev_line)
+						scope = self.extract_bold_italic(scope)
+						self.create_node(
+							[scope],
+							node_type='tag',
+							attributes={'name': 'bold-italic'}
+						)
+					elif scope_type=='bold':
+						self.creat_simple_string(prev_line)
+						scope = self.extract_bold(scope)
+						self.create_node(
+							[scope],
+							node_type='tag',
+							attributes={'name': 'bold'}
+						)
+					elif scope_type=='italic':
+						self.creat_simple_string(prev_line)
+						scope = self.extract_italic(scope)
+						self.create_node(
+							[scope],
+							node_type='tag',
+							attributes={'name': 'italic'}
+						)
+					else:
+						# simple-string
+						self.node_type='tag'
+						self.attributes={'name': 'simple-string'}
+						self.string_lines.append(line)
+						line=''
+		else:
+			print(f"String is not exists.")
 
 	# ------------------------------- static methods ---------------------------
+
+	@staticmethod
+	def extract_href(string_line:str):
+		match_in = re.match(r'\[(.*?)\]\((.*?)\)', string_line)
+		return match_in.group(1), match_in.group(2)
+
+	@staticmethod
+	def extract_tt_quote(string_line:str, mode:str):
+		if mode=='tt-quote':
+			return re.match(r"''(.*?)''", string_line).group(1)
+		else:
+			return re.match(r"`(.*?)`", string_line).group(1)
+
+	@staticmethod
+	def extract_bold_italic(string_line:str):
+		return re.match(r"\*\*\*(.*?)\*\*\*", string_line).group(1)
+
+	@staticmethod
+	def extract_bold(string_line:str):
+		return re.match(r"\*\*(.*?)\*\*", string_line).group(1)
+
+	@staticmethod
+	def extract_italic(string_line:str):
+		return re.match(r"\*(.*?)\*", string_line).group(1)
+
+	@staticmethod
+	def find_separate_scope(string_line:str):
+		# Get string. Return scope_type, scope, other_string
+		anchor = re.match(r'^\[:.*?\]$', string_line)
+		image = re.match(r'^\[%image%\]\(.*?\)$', string_line)
+		hr = re.match(r'^\[%hr%\]$', string_line)
+		if anchor!=None:
+			return 'anchor', string_line, ''
+		elif image!=None:
+			return 'image', string_line, ''
+		elif hr!=None:
+			return 'hr', string_line, ''
+		else:
+			return None, None, string_line
+
+	@staticmethod
+	def find_overlap_scope(string_line:str):
+		# Get string. Return scope_type, scope, other_string
+		maximal = len(string_line)+1
+		mini_data_base = {
+			"scope-name": [
+				'tt-quote',
+				'back-apostroph',
+				'bold-italic',
+				'bold',
+				'italic',
+				'hyperlink'
+			],
+			"scope-regexp":
+			[
+				re.search(r'(\'\')(.*?)(\1)', string_line),
+				re.search(r'(`)(.*?)(\1)', string_line),
+				re.search(r'(\*\*\*)(.*?)(\1)', string_line),
+				re.search(r'(\*\*)(.*?)(\1)', string_line),
+				re.search(r'(\*)(.*?)(\1)', string_line),
+				re.search(r'\[(.*?)\]\((.*?)\)', string_line)
+			],
+			"scope-instring":
+			[]
+		}
+		for string_id in mini_data_base['scope-name']:
+			i = mini_data_base['scope-name'].index(string_id)
+			match_in = mini_data_base['scope-regexp'][i]
+			mini_data_base['scope-instring'].append(
+				string_line.index(match_in.group(0)) if match_in is not None else maximal)
+		minimal = min(mini_data_base['scope-instring'])
+		if minimal!=maximal:
+			i = mini_data_base['scope-instring'].index(minimal)
+			scope_type = mini_data_base['scope-name'][i]
+			scope = mini_data_base['scope-regexp'][i].group(0)
+			q = string_line.index(scope)
+			prev_line = string_line[0:q]
+			post_line = string_line[q+len(scope):]
+			return scope_type, prev_line, scope, post_line
+		else:
+			return None, '', '', string_line
+
+	@staticmethod
+	def match_in_string(string_line:str):
+		match_in = re.search(r'((\'\'|`|\*\*\*|\*\*|\*)(.*?)(\2)|\[.*?\]\(.*?\))', string_line)
+		return True	if match_in is not None else False
 
 	@staticmethod
 	def extract_anchor(string_line):
@@ -408,7 +602,7 @@ class NewNode():
 		if direct == "cyr_to_lat":
 			i = 0
 			for letter in translator['cyr']:
-				string_line = string_line.replace(translator['cyr'][i], translator['lat'][i])
+				string_line = string_line.lower().replace(translator['cyr'][i], translator['lat'][i])
 				i+=1
 			string_line = re.sub(r'[^\w\-\s]', '', string_line)
 		return string_line
@@ -475,6 +669,10 @@ class NewNode():
 	@staticmethod
 	def clear_li_string(string_line:str):
 		return re.match(r'^(\s*(\*|\d+\.)\s+)(.+?)$', string_line).group(3)
+
+	@staticmethod
+	def get_code_type(string_line:str):
+		return re.match(r'^\s*```(\w+)\s*$', string_line).group(1)
 
 def main():
 	# названия файлов, из которых берём сборку
