f7e9a3abdb4ff9086b3345a489c142d5d4a15843
aleksversus <aleksversus@mail.ru>
2023-03-07 00:16:19 +0500

    конечный вид конвертера

---
+++ res/[converters]/build_main.py
@@ -0,0 +1,15 @@
+from nodes import (TextToHTML, NewDataBase, NewFolder, NewFile, NewNode)
+
+def main():
+	# названия файлов, из которых берём сборку
+	html_json=[
+		# "..\\..\\[source]\\example\\html.json",
+		"..\\..\\[source]\\готовые статьи\\build-project.json",
+		# "..\\..\\[source]\\ответы\\html.json",
+		# "..\\..\\[source]\\вики-qsp\\html.json"
+	]
+	for path in html_json:
+		TextToHTML(path).convert_to()
+
+if __name__=="__main__":
+	main()
