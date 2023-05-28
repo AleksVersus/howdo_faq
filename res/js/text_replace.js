25869f62668d2dbe8111b482c63051abe022e8cc
aleksversus <aleksversus@mail.ru>
2023-05-28 14:29:06 +0500

    refactor: replace styles in articles for new-styles list

--- res/js/text_replace.js
+++ res/js/text_replace.js
@@ -1,13 +1,12 @@
 /*eslint-env browser*/
 
 /* Исправляем год в штампе */
-var ds = new Date();
-var stamp_faq = document.getElementById("date_stamp_faq");
-var stamp_gamrus = document.getElementById("date_stamp_gamrus");
-stamp_faq.innerHTML = ds.getFullYear();
-stamp_gamrus.innerHTML = ds.getFullYear();
+const date = new Date();
+document.querySelectorAll('.subscribe-year').forEach((span) => {
+	span.innerHTML = date.getFullYear();
+});
 
-/* Исправляем заголовок */
-var title = document.title;
-var head = document.body.getElementsByTagName('h2')[0];
-document.title = title + " - "+head.innerHTML;
+// /* Исправляем заголовок */
+// var title = document.title;
+// var head = document.body.getElementsByTagName('h2')[0];
+// document.title = title + " - "+head.innerHTML;
