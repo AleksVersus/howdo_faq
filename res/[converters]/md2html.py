df907ff29c0460c954e67dd1772ed84f6aeacc10
aleksversus <aleksversus@mail.ru>
2024-07-25 02:26:01 +0500

    refacotr: conv to markdown

---
+++ res/[converters]/md2html.py
@@ -0,0 +1,8 @@
+import markdown
+
+markdown.markdownFromFile(
+	input = '..\\..\\[source]\\ответы\\00_intro\\00_vstuplenie_0000.md',
+	output = 'output.html',
+	encoding = 'utf8'
+)
+# отстой. Криво парсит маркдаун из обсидиана.
