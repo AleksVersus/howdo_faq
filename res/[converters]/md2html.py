e0a0f2947607921c2f5f37d44089f2ad78851c2b
aleksversus <aleksversus@mail.ru>
2024-07-28 14:40:18 +0500

    refactor: replace headers and anchors

--- res/[converters]/md2html.py
+++
@@ -1,8 +0,0 @@
-import markdown
-
-markdown.markdownFromFile(
-	input = '..\\..\\[source]\\ответы\\00_intro\\00_vstuplenie_0000.md',
-	output = 'output.html',
-	encoding = 'utf8'
-)
-# отстой. Криво парсит маркдаун из обсидиана.
