53c77f81fd2e3c8ca431876554521235efb8677b
aleksversus <aleksversus@mail.ru>
2026-02-18 22:32:32 +0500

    fix(docs): empty link in article transition_570_at_59x

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -29,7 +29,6 @@ const config: Config = {
   projectName: 'howdo_faq', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
